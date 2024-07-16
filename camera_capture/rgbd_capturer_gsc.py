5447157215a40ec11033efc4e5d46ece5cb765d9
Sangboom <saturnbum@naver.com>
2024-07-16 13:23:32 +0900

    feat: remove scene & add back to previous cut

--- camera_capture/rgbd_capturer_gsc.py
+++ camera_capture/rgbd_capturer_gsc.py
@@ -8,7 +8,7 @@ from pyk4a import Config, PyK4A, ColorResolution, DepthMode, ImageFormat, FPS
 # this code is for capturing RGB and Depth images using py4ka and azure kinect camera
 def capture_rgbd(args):
     group_id = args.group_id
-    scene_id = args.scene_id
+    # scene_id = args.scene_id
     cut_id = args.cut_id
     
     # check if the save directory exists (group_id)
@@ -16,17 +16,11 @@ def capture_rgbd(args):
         os.makedirs(os.path.join(args.save_dir, 'rgb', f'group_{group_id:06d}'))
     if not os.path.exists(os.path.join(args.save_dir, 'depth', f'group_{group_id:06d}')):
         os.makedirs(os.path.join(args.save_dir, 'depth', f'group_{group_id:06d}'))
-    
-    # check if the save directory exists (scene_id)
-    if not os.path.exists(os.path.join(args.save_dir, 'rgb', f'group_{group_id:06d}/scene_{scene_id:06d}')):
-        os.makedirs(os.path.join(args.save_dir, 'rgb', f'group_{group_id:06d}/scene_{scene_id:06d}'))
-    if not os.path.exists(os.path.join(args.save_dir, 'depth', f'group_{group_id:06d}/scene_{scene_id:06d}')):
-        os.makedirs(os.path.join(args.save_dir, 'depth', f'group_{group_id:06d}/scene_{scene_id:06d}'))
 
     rgb_save_dir = os.path.join(args.save_dir, 'rgb')
     depth_save_dir = os.path.join(args.save_dir, 'depth')
 
-    cv2_window_name = 'rgbd - group_id: ' + str(group_id) + ' scene_id: '+ str(scene_id) + ' cut_id: '+ str(cut_id) + '  ' + 'C: Save & Next Cut / S: Next Scene / G: Next Group / Q: Quit'
+    cv2_window_name = 'rgbd - group_id: ' + str(group_id) + ' cut_id: '+ str(cut_id) + '  ' + 'C: Save & Next Cut / X: Previous Cut / G: Next Group / Q: Quit'
     k4a0 = PyK4A(Config(color_resolution=ColorResolution.RES_1080P,
                        depth_mode=DepthMode.WFOV_UNBINNED,
                        camera_fps=FPS.FPS_5,
@@ -54,27 +48,24 @@ def capture_rgbd(args):
         depth_vis0 = cv2.resize(depth_vis0, (1920, 1080))
         rgbd_img0 = np.hstack([color_image0, depth_vis0])
         cv2.imshow(cv2_window_name, rgbd_img0)
-        print('group {} / scene {} / cut {}'.format(group_id, scene_id, cut_id))
+        print('group {} / cut {}'.format(group_id, cut_id))
         
         # Save RGBD Image & Move to Next Cut
         if key == ord('c'):
             # save color image to the save directory with scene_id_000000.png
             # save depth image to teh save directory with scene_id_000000.npy
             # for img0
-            cv2.imwrite(os.path.join(rgb_save_dir, f'group_{group_id:06d}/scene_{scene_id:06d}/{cut_id:06d}.png'), color_image0)
-            np.save(os.path.join(depth_save_dir, f'group_{group_id:06d}/scene_{scene_id:06d}/{cut_id:06d}.npy'), depth_image0)
-            print(f'{group_id:06d}_{scene_id:06d}_{cut_id:06d} RGBD image saved')
+            cv2.imwrite(os.path.join(rgb_save_dir, f'group_{group_id:06d}/{cut_id:06d}.png'), color_image0)
+            np.save(os.path.join(depth_save_dir, f'group_{group_id:06d}/{cut_id:06d}.npy'), depth_image0)
+            print(f'{group_id:06d}_{cut_id:06d} RGBD image saved')
             cut_id += 1
         
-        # Move to Next Scene
-        elif key == ord('s'):
-            cut_id = 0
-            scene_id += 1
-            # check if the save directory exists (scene_id)
-            if not os.path.exists(os.path.join(args.save_dir, 'rgb', f'group_{group_id:06d}/scene_{scene_id:06d}')):
-                os.makedirs(os.path.join(args.save_dir, 'rgb', f'group_{group_id:06d}/scene_{scene_id:06d}'))
-            if not os.path.exists(os.path.join(args.save_dir, 'depth', f'group_{group_id:06d}/scene_{scene_id:06d}')):
-                os.makedirs(os.path.join(args.save_dir, 'depth', f'group_{group_id:06d}/scene_{scene_id:06d}'))
+        # Go Back to Previous Cut
+        elif key == ord('x'):
+            if cut_id > 0:
+                cut_id -= 1
+            else:
+                print("Cut id is 0!! Can't go back to previous cut")
             
         # Move to Next Group
         elif key == ord('g'):
@@ -87,12 +78,6 @@ def capture_rgbd(args):
             if not os.path.exists(os.path.join(args.save_dir, 'depth', f'group_{group_id:06d}')):
                 os.makedirs(os.path.join(args.save_dir, 'depth', f'group_{group_id:06d}'))
             
-            # check if the save directory exists (scene_id)
-            if not os.path.exists(os.path.join(args.save_dir, 'rgb', f'group_{group_id:06d}/scene_{scene_id:06d}')):
-                os.makedirs(os.path.join(args.save_dir, 'rgb', f'group_{group_id:06d}/scene_{scene_id:06d}'))
-            if not os.path.exists(os.path.join(args.save_dir, 'depth', f'group_{group_id:06d}/scene_{scene_id:06d}')):
-                os.makedirs(os.path.join(args.save_dir, 'depth', f'group_{group_id:06d}/scene_{scene_id:06d}'))
-        
         # Quit
         elif key == ord('q'):
             break
@@ -105,7 +90,6 @@ def capture_rgbd(args):
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument('--group_id', type=int, default=0)
-    argparser.add_argument('--scene_id', type=int, default=0)
     argparser.add_argument('--cut_id', type=int, default=0)
     
     argparser.add_argument('--save_dir', type=str, default='data/home') # 'data/home' or 'data/factory'
